0d4ad4ed063d3377c83b70e4ec5dc1942053fae6
egb-dev <elias@lfpost.dk>
2019-10-16 17:05:57 +0200

    A lot of stuff

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>This is our app</h1>
+      <h1>This is our app</h1>
     </div>
   );
 }
 
 export default App;
+
+
+  // {cardList.cards.map(card => (
+  //   <h3>{card.title}</h3>
+  //   <img src={card.img.src} alt={card.img.alt}/>
+  // ))}
+
+  // #3d4669
