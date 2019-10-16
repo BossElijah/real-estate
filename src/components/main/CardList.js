a10cb171bbed29ffa3f854f7d886345af8c6e36c
egb-dev <elias@lfpost.dk>
2019-10-16 18:28:54 +0200

    Create cardlist component

--- src/components/main/CardList.js
+++ src/components/main/CardList.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CardList = () => {
-  return (
-    <div>
-      Cardlist
-    </div>
-  );
+const CardList = ({ cards }) => {
+  return cards.map(card => (
+    <>
+      <Link to={card.linkSrc}>
+        <h3>{card.title}</h3>
+        <img src={process.env.PUBLIC_URL + '/img/' + card.img.src} alt={card.img.alt} style={{ width: '25vw' }} />
+      </Link>
+    </>
+  ));
 };
 
 export default CardList;
