3862bb3179f3d5a36fa9f495c55112addf21978d
Daniel David Kristiansen <daniel@lfpost.dk>
2019-10-16 18:51:33 +0200

    image styling

--- src/components/main/Hero.js
+++ src/components/main/Hero.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Hero = ({ title, img: { src, alt }  }) => {
-  console.log('/public/img/' + src);
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={process.env.PUBLIC_URL + '/img/' + src} alt={alt} style={{ width: '100%' }} />
+    <div className="hero-container" style={{ background:  `center / contain no-repeat url(${process.env.PUBLIC_URL + '/img/' + src})` }} >
+      <h1 className="hero-title">{title}</h1>
     </div>
   );
 };
