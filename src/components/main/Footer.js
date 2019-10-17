b960269240def39cc6f292074b5017e7f0fc534b
Daniel David Kristiansen <daniel@lfpost.dk>
2019-10-17 11:49:10 +0200

    some css

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <>
-    <div>
-      <h2>Tilmeld dig vores nyhedsbrev</h2>
-      <input type="text" name="email" id="email" placeholder="Din email-adresse her..." />
-      <a href="/nyhedsbrev" className="btn">Tilmeld</a>
+    <div className="footer">
+       <div className="newsletter">
+          <h2 className="footer__title">Tilmeld dig vores nyhedsbrev</h2>
+          <input type="text" name="email" id="email" placeholder="Din email-adresse her..." />
+          <a href="/nyhedsbrev" className="btn"><b>Tilmeld</b></a>
+        </div>
+      <p className="title__bottom">Real Estate &copy; 2019 All Rights Reserved</p>
     </div>
-    <p>Real Estate &copy; 2019 All Rights Reserved</p>
-    </>
   );
 };
 
