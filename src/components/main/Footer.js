b4357bfc376385c2939564c343680c9186011524
egb-dev <elias@lfpost.dk>
2019-10-17 14:32:40 +0200

    Make newsletter link open in new tab

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className="footer">
-       <div className="newsletter">
-          <h2 className="footer__title">Tilmeld dig vores nyhedsbrev</h2>
-          <input type="text" name="email" id="email" placeholder="Din email-adresse her..." />
-          <Link to="/nyhedsbrev" className="btn footer__btn"><b>Tilmeld</b></Link>
-        </div>
-      <p className="title__bottom">Real Estate &copy; 2019 All Rights Reserved</p>
+      <div className="newsletter">
+        <h2 className="footer__title">Tilmeld dig vores nyhedsbrev</h2>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Din email-adresse her..."
+        />
+        <Link to="/nyhedsbrev" target="_blank" className="btn footer__btn">
+          <b>Tilmeld</b>
+        </Link>
+      </div>
+      <p className="title__bottom">
+        Real Estate &copy; 2019 All Rights Reserved
+      </p>
     </div>
   );
 };
