3207d60104a20d44de48049108ef11d3874db783
egb-dev <elias@lfpost.dk>
2019-10-17 12:17:32 +0200

    Style footer

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
        <div className="newsletter">
           <h2 className="footer__title">Tilmeld dig vores nyhedsbrev</h2>
           <input type="text" name="email" id="email" placeholder="Din email-adresse her..." />
-          <a href="/nyhedsbrev" className="btn"><b>Tilmeld</b></a>
+          <a href="/nyhedsbrev" className="btn footer__btn"><b>Tilmeld</b></a>
         </div>
       <p className="title__bottom">Real Estate &copy; 2019 All Rights Reserved</p>
     </div>
