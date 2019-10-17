baff6a0591285499184fcb983e31275ce09383bb
egb-dev <elias@lfpost.dk>
2019-10-17 14:59:30 +0200

    Make newsletter button close current tab

--- src/components/main/TextMedia.js
+++ src/components/main/TextMedia.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const TextMedia = ({ title, slogan, text, cta: { url, ctaText } }) => {
+const TextMedia = ({ title, text }) => {
   return (
     <>
-    <div className="newsletter">
-      <div>
-        <h2 className="newsletter__title">{title}</h2>
+      <div className="newsletter">
+        <div>
+          <h2 className="newsletter__title">{title}</h2>
+        </div>
+        <div>
+          <p className="newsletter__text">{text}</p>
+        </div>
+        <a href="javascript:window.open('','_self').close();" className="btn-l">Tilbage</a>
       </div>
-      <div>
-        <p className="newsletter__text">{text}</p>
-      </div>
-      <Link to={url} className="btn-l">
-      {ctaText}
-      </Link>
-    </div>
     </>
   );
 };
