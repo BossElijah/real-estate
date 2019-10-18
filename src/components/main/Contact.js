f084c1272c3438c75066f0ad420185e29dff2876
egb-dev <elias@lfpost.dk>
2019-10-18 21:44:24 +0200

    Cleanup Contact.js

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Contact = ({ img: {src, alt}, title, name, subtitle, contacts }) => {
+const Contact = ({ img: {src, alt}, title, name, contacts }) => {
   return (
     <div className="contact">
       <div className="contact__image-wrapper">
@@ -9,7 +9,7 @@ const Contact = ({ img: {src, alt}, title, name, subtitle, contacts }) => {
       <div className="contact__info">
         <h2 className="contact__title">{title}</h2>
         <h3 className="contact__name">{name}</h3>
-        {contacts.map((contact, index) => (
+        {contacts.map((contact) => (
           <div className="icons">
             <i className={contact.icon + ' contact__icon'}></i>
             <p className="icon__text">{contact.text}</p>
