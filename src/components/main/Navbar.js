09bba0d69acf9b9027d236638ae37b9fece4ec15
egb-dev <elias@lfpost.dk>
2019-10-18 22:08:41 +0200

    Change logo

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <Fragment>
       <Link to="/">
-        <img src={process.env.PUBLIC_URL + '/img/logo.svg'} alt="Logo" />
+        <img src={process.env.PUBLIC_URL + '/img/logo.png'} alt="Logo" />
       </Link>
     </Fragment>
   );
